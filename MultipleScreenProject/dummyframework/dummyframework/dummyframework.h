833c52847b17735947f08ec443db2f6efce04f26
Ananya Joshi <jananya@vmware.com>
2021-03-12 10:26:25 +0530

    finsished

---
+++ MultipleScreenProject/dummyframework/dummyframework/dummyframework.h
@@ -0,0 +1,18 @@
+//
+//  dummyframework.h
+//  dummyframework
+//
+//  Created by Ananya Joshi on 12/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for dummyframework.
+FOUNDATION_EXPORT double dummyframeworkVersionNumber;
+
+//! Project version string for dummyframework.
+FOUNDATION_EXPORT const unsigned char dummyframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <dummyframework/PublicHeader.h>
+
+
---
+++ MultipleScreenProject/dummyframework/logic.swift
@@ -0,0 +1,8 @@
+//
+//  logic.swift
+//  dummyframework
+//
+//  Created by Ananya Joshi on 12/03/21.
+//
+
+import Foundation
